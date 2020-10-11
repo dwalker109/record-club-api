b708d34c463afd509e6b0c79e166736ca6c54138
Dan Walker <dwalker109@gmail.com>
2020-10-11 02:10:40 +0100

    cleaner api error messaging

--- lib/api/error.go
+++ lib/api/error.go
@@ -6,17 +6,19 @@ import (
 )
 
 type ErrorResponse struct {
-	HttpStatusCode int
-	Err            error
+	HttpStatusCode int    `json:"-"`
+	Err            error  `json:"-"`
+	Msg            string `json:"msg"`
 }
 
 func (e *ErrorResponse) Render(w http.ResponseWriter, r *http.Request) error {
 	render.Status(r, e.HttpStatusCode)
+	e.Msg = e.Err.Error()
 	return nil
 }
 
 type AppErrString string
 
 func NewErrorResponse(status int, err error) render.Renderer {
-	return &ErrorResponse{status, err}
+	return &ErrorResponse{status, err, "unknown error"}
 }
